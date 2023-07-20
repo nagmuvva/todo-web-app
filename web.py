2a971f1946d2202b9d41b1b9f6cb60f5c11c6aaa
nagmuvva <nagmuvva@gmail.com>
2023-07-20 10:53:44 -0400

    some experiments

--- web.py
+++ web.py
@@ -10,18 +10,19 @@ def add_todo():
     functions.save_todos(todos)
 
 
+st.set_page_config(layout="wide")
+
 st.title("My Todo App")
 st.subheader("This is my todo app.")
-st.write("This is to increase your productivity")
+st.write("This is to increase your <b>productivity</b>.", unsafe_allow_html=True)
 st.write("Select to delete")
 
 for index, todo in enumerate(todos):
-    checkbox = st.checkbox(todo, key = todo)
+    checkbox = st.checkbox(todo, key=todo)
     if checkbox:
         todos.pop(index)
         functions.save_todos(todos)
         del st.session_state[todo]
         st.experimental_rerun()
 
-st.text_input(label="Enter a todo", placeholder="Add new todo...",  on_change=add_todo, key="new_todo",)
-
+st.text_input(label="Enter a todo", placeholder="Add new todo...", on_change=add_todo, key="new_todo", )
